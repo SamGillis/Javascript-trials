e45121863b3e3bb800d6eae2793293817e8fb1c0
Sam Gillis <sbvb13@gmail.com>
2020-10-27 21:01:10 +0000

    Finished functions in objects.js
    Co-authored-by: jadele511 <baongocle1991@gmail.com>

--- js-trials-2/objects.js
+++ js-trials-2/objects.js
@@ -4,10 +4,34 @@
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+  let wordCount = {};
+
+  for (const word of phrase.split(' ')) {
+    if (wordCount[word]) {
+      wordCount[word] += 1;
+    } else {
+      wordCount[word] = 1;
+    }
+  }
+  return wordCount;
 }
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
   // Replace this with your code
+  
+  const melonPrices = {
+      2.50: ['Cantaloupe', 'Honeydew'],
+      2.95: ['Watermelon'],
+      3.25: ['Musk', 'Crenshaw'],
+      14.25: ['Christmas']
+  };
+  
+  if (!melonPrices[price]) {
+    return; 
+  }
+
+  return melonPrices[price].sort();
+
 }
